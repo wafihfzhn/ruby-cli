c8dd169eadd4d718d4be30e622eb460fff53aa07
wafihfzhn <hafizhuddin96@gmail.com>
2021-08-25 01:02:57 +0800

    Add logic for check directory

--- dir.rb
+++ dir.rb
@@ -1,15 +1,19 @@
 puts 'Please enter your directory ?'
 dir = gets.strip
 
-files = Dir.glob(File.join(dir, '**', '*')).select { |file| File.file?(file) }
-files_contents = [] 
+if Dir.exist?(dir)
+  files = Dir.glob(File.join(dir, '**', '*')).select { |file| File.file?(file) }
+  files_contents = [] 
 
-files.each do |file|
-  File.foreach(file) do |content|
-    files_contents << content
+  files.each do |file|
+    File.foreach(file) do |content|
+      files_contents << content
+    end
   end
-end
 
-files_contents.tally.each do |key, value|
-  puts "#{key} #{value}"
+  files_contents.tally.each do |key, value|
+    puts "#{key} #{value}"
+  end
+else
+  puts 'Your directory not found'
 end
