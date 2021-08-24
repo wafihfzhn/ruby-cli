b4c24492d2af3693cb760f4d6404771c8d44e0ee
wafihfzhn <hafizhuddin96@gmail.com>
2021-08-25 00:34:58 +0800

    Group files with the same content

--- dir.rb
+++ dir.rb
@@ -1,9 +1,14 @@
 dir = '/Users/wafihfzhn/Downloads/DropsuiteTest'
 
 files = Dir.glob(File.join(dir, '**', '*')).select { |file| File.file?(file) }
+files_contents = [] 
 
 files.each do |file|
   File.foreach(file) do |content|
-    puts content
+    files_contents << content
   end
+end
+
+files_contents.tally.each do |key, value|
+  puts "#{key} #{value}"
 end
