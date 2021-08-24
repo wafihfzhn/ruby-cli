e8000e29048ee2e26a1e6b1a16b29dd5c3a005ee
wafihfzhn <hafizhuddin96@gmail.com>
2021-08-25 00:22:49 +0800

    Check contents of each file

--- dir.rb
+++ dir.rb
@@ -2,4 +2,8 @@ dir = '/Users/wafihfzhn/Downloads/DropsuiteTest'
 
 files = Dir.glob(File.join(dir, '**', '*')).select { |file| File.file?(file) }
 
-puts files
+files.each do |file|
+  File.foreach(file) do |content|
+    puts content
+  end
+end
